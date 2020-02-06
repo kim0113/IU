dcbb3bcac16e2eeec53be657152f6441856dfdae
kim0113 <59689748+kim0113@users.noreply.github.com>
2020-02-06 20:11:17 +0800

    Add files via upload

---
+++ IU/script.js
@@ -0,0 +1,12 @@
+function showmessage(){
+    alert("Hi, lets learn Javascript!");
+for(var i = 5;i>0;i--){
+    alert("Variable I is now: " + i);
+    }
+}
+
+function getdouble(){
+    var num = document.getElementById("number").value;
+    var totel = document.getElementsByTagName("t");
+    alert(totel.length);
+}
